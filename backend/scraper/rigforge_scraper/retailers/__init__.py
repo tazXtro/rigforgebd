58fa9e043f6b4515c193b919b6585b6aca79112e
Md. Tazrian Hasnat <tazrian.hasnat@northsouth.edu>
2026-01-16 11:40:09 +0600

    Revert "Playwright implementation"
    This reverts commit 0b7f723fb06d7ea2c4d8695e2c7fc8462ae5677a.

--- backend/scraper/rigforge_scraper/retailers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Retailer registry package.
-
-Provides centralized configuration for all retailer spiders.
-"""
-from .registry import RETAILERS, get_retailer_config, get_enabled_retailers
-
-__all__ = ['RETAILERS', 'get_retailer_config', 'get_enabled_retailers']
